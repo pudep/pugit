ebec7668541d31537f452286ecfaf819a745d54e
pudep <261843105+pudep@users.noreply.github.com>
2026-07-22 08:01:18 +0530

    removed wrong syntax

--- src/git/refresh.rs
+++ src/git/refresh.rs
@@ -9,7 +9,8 @@ fn refresh(repo: &Repository, signal: WatchSignals) -> anyhow::Result<()> {
     let head = Git::get_current_head(repo)?;
     let branch = Git::get_current_local_branch(repo, &head);
     let oid = Git::get_oid_current(repo, &branch)?;
-    let upstream = Git::get_current_upstream(&branch)?;
+    let upstream = Git::get_current_upstream(&branch);
   }
+
   Ok(())
 }
