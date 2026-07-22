6672b1a357678ba0520f7f986f7827f005d8a00b
pudep <261843105+pudep@users.noreply.github.com>
2026-07-22 17:06:23 +0530

    added local

--- src/bin/git.rs
+++ src/bin/git.rs
@@ -1,10 +1,14 @@
-use pugit::git::Git;
+use pugit::git::{Git, current::local::Local};
 
 fn main() -> anyhow::Result<()> {
   let git = Git::new("~/impl/rust/pugit/")?;
 
   if git.head.is_refrence() {
-    println!("It is head")
+    match Git::get_current_local_branch(&git.head, &git.repo)? {
+      Local::Branch(name) => println!("{}", name),
+      Local::Error(error) => println!("{}", error),
+      Local::None => println!("None"),
+    }
   }
   Ok(())
 }
